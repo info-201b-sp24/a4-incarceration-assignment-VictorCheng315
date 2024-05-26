7abb204bcadbc22866e67952cdb49f6c9acc8993
VictorCheng315 <bowen.cheng315@hotmail.com>
2024-05-26 14:12:00 -0700

    This assignment is completed

---
+++ summary.R
@@ -0,0 +1,14 @@
+# The problem domain I am exploring is the distribution of incarceration rates across different races in the Washington State. 
+# The variables I choose are aapi_prison_pop_rate, black_prison_pop_rate, latinx_prison_pop_rate, native_prison_pop_rate, 
+# and white_prison_pop_rate. The reason I choose these variables is that by directly comparing these variables that represents 
+# different races of people, we can better understand the incarceration rates of different racial groups and compare them to 
+# see if there is potential discrimination and unfairness.
+
+
+# By looking at the summary information of incarceration of each race, it is easy to find out that black people have the 
+# highest mean, which is 1922.378	per 100,000 people per county, and it is way higher than the second highest mean, which 
+# is 970.8833 for native people. Moreover, black people also has the highest max(8215.3) and median(1738.77). Comparing with 
+# Asian American and Pacific Islander people, which only has 57.88149 for mean, 675.68 for max, and 29.785 for median. These 
+# are just some general information, but the huge gap between different races, and we can clearly see that black people are 
+# incarcerated at disproportionately high rates across time, the highest incarceration mean rate for black people is 2390.614, 
+# which is in 1999, and the lowest is 1366.395 in 2012.
